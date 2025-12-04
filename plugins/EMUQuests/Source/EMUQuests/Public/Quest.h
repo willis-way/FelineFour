4304679d6eb6f408aebb39559cb053e645e5adca
rachelgowell-lab <rachel.gowell@gmail.com>
2025-12-04 11:14:48 -0500

    plug in in another spot

---
+++ plugins/EMUQuests/Source/EMUQuests/Public/Quest.h
@@ -0,0 +1,76 @@
+﻿// EMU Intro to UE5. Thomas
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "QuestTypes.h"
+#include "Quest.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnQuestStatusChanged, const FString&, QuestID, EQuestStatus, NewStatus);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnObjectiveUpdated, int32, ObjectiveIndex, const FQuestObjective&, Objective);
+
+UCLASS(BlueprintType, Blueprintable)
+class QUESTSYSTEM_API UQuest : public UObject
+{
+    GENERATED_BODY()
+
+public:
+    UQuest();
+
+    UFUNCTION(BlueprintCallable, Category = "Quest")
+    void InitializeQuest(const FQuestData& QuestData);
+
+    UFUNCTION(BlueprintCallable, Category = "Quest")
+    void StartQuest();
+
+    UFUNCTION(BlueprintCallable, Category = "Quest")
+    void CompleteQuest();
+
+    UFUNCTION(BlueprintCallable, Category = "Quest")
+    void FailQuest();
+
+    UFUNCTION(BlueprintCallable, Category = "Quest")
+    void TurnInQuest();
+
+    UFUNCTION(BlueprintCallable, Category = "Quest")
+    void UpdateObjectiveProgress(int32 ObjectiveIndex, int32 Amount = 1);
+
+    UFUNCTION(BlueprintCallable, Category = "Quest")
+    void UpdateObjectiveByType(EObjectiveType ObjectiveType, const FString& TargetID, int32 Amount = 1);
+
+    UFUNCTION(BlueprintPure, Category = "Quest")
+    bool IsObjectiveCompleted(int32 ObjectiveIndex) const;
+
+    UFUNCTION(BlueprintPure, Category = "Quest")
+    bool AreAllObjectivesCompleted() const;
+
+    UFUNCTION(BlueprintPure, Category = "Quest")
+    float GetCompletionPercentage() const;
+
+    // Getters
+    UFUNCTION(BlueprintPure, Category = "Quest")
+    const FQuestData& GetQuestData() const { return QuestData; }
+
+    UFUNCTION(BlueprintPure, Category = "Quest")
+    EQuestStatus GetQuestStatus() const { return QuestStatus; }
+
+    UFUNCTION(BlueprintPure, Category = "Quest")
+    const TArray<FQuestObjective>& GetObjectives() const { return QuestData.Objectives; }
+
+    // Delegates
+    UPROPERTY(BlueprintAssignable)
+    FOnQuestStatusChanged OnQuestStatusChanged;
+
+    UPROPERTY(BlueprintAssignable)
+    FOnObjectiveUpdated OnObjectiveUpdated;
+
+private:
+    UPROPERTY()
+    FQuestData QuestData;
+
+    UPROPERTY()
+    EQuestStatus QuestStatus;
+
+    void CheckQuestCompletion();
+    void SetQuestStatus(EQuestStatus NewStatus);
+};
---
+++ plugins/EMUQuests/Source/EMUQuests/Public/QuestGiver.h
@@ -0,0 +1,47 @@
+﻿// EMU Intro to UE5 Thomas
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.h"
+#include "GameFramework/Character.h"
+#include "QuestGiver.generated.h"
+
+class UWidgetComponent;
+class USphereComponent;
+
+UCLASS()
+class QUESTSYSTEM_API AQuestGiver : public ACharacter, public IInteractable
+{
+	GENERATED_BODY()
+
+public:
+	AQuestGiver();
+
+	virtual void InteractWith(const class AQuestCharacter* Character) override;
+	
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OpenQuestDialog(const class AQuestCharacter* Character);
+	
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Quest Giver", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USphereComponent> SphereComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Quest Giver", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UWidgetComponent> WidgetComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Quest Giver", meta = (AllowPrivateAccess = "true"))
+	FName QuestID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest Giver", meta = (AllowPrivateAccess = "true"))
+	bool bQuestTaken = false;
+};
